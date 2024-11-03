661c77bed72857a273035e411a62308f160deab6
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-03 16:22:59 +0100

    - added tsdocs

--- src/routes/whitelistRoutes.ts
+++ src/routes/whitelistRoutes.ts
@@ -4,8 +4,31 @@ import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = Router();
 
+/**
+ * Adds a user to the whitelist.
+ * @route POST /api/whitelist
+ * @middleware authenticateToken - Middleware to authenticate the token.
+ * @param {Request} req - The request object containing the incoming HTTP request.
+ * @param {Response} res - The response object used to send a response to the client.
+ */
 router.post('/api/whitelist', authenticateToken, addToWhitelist);
+
+/**
+ * Retrieves all users from the whitelist.
+ * @route GET /api/whitelist
+ * @middleware authenticateToken - Middleware to authenticate the token.
+ * @param {Request} req - The request object containing the incoming HTTP request.
+ * @param {Response} res - The response object used to send a response to the client.
+ */
 router.get('/api/whitelist', authenticateToken, getWhitelist);
+
+/**
+ * Deletes a user from the whitelist.
+ * @route DELETE /api/whitelist/:username
+ * @middleware authenticateToken - Middleware to authenticate the token.
+ * @param {Request} req - The request object containing the incoming HTTP request.
+ * @param {Response} res - The response object used to send a response to the client.
+ */
 router.delete('/api/whitelist/:username', authenticateToken, deleteFromWhitelist);
 
 export default router;
