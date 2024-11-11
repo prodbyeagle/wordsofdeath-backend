83193d0cc9aab73fc6427c2be7f95fd0d9624290
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-11-11 12:51:06 +0100

    updated the logic to check for admin and owner.

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -9,7 +9,7 @@ import { log } from '../utils/logger';
 dotenv.config();
 
 /**
- * Handles the Check from the User for admin authentication.
+ * Handles the check for user admin authentication.
  * @param req - The request object containing the incoming HTTP request.
  * @param res - The response object used to send a response to the client.
  */
@@ -45,19 +45,19 @@ export const checkAdmin = async (req: Request, res: Response) => {
       const user = await db.collection('users').findOne({ username: username });
       log("debug", `Retrieved user from database: ${JSON.stringify(user)}`);
 
-      if (user) {
-         log("debug", `User roles: ${user.roles}`);
-      } else {
+      if (!user) {
          log("warn", `User not found in the database: ${username}`);
          return res.status(404).json({ message: "User not found." });
       }
 
-      if (user.roles && user.roles.includes("admin")) {
-         log("info", `User is an admin: ${username}`);
+      log("debug", `User roles: ${user.roles}`);
+
+      if (user.roles && (user.roles.includes("admin") || user.roles.includes("owner"))) {
+         log("info", `User has admin or owner privileges: ${username}`);
          return res.json({ isAdmin: true });
       } else {
-         log("info", `Access denied: User is not an admin: ${username}`);
-         return res.status(403).json({ message: "Access denied: User is not an admin." });
+         log("info", `Access denied: User does not have admin privileges: ${username}`);
+         return res.status(403).json({ message: "Access denied: User does not have admin privileges." });
       }
    } catch (error) {
       log("error", `An error occurred while checking admin status: ${error}`);
